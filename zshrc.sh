4a686f8850f7bd20a544a4200ba52f06cd1d0800
James Ramsay <james@jramsay.com.au>
2021-02-06 21:38:25 +1100

    zsh: fix fzf integration
    Homebrew uses a different path on Apple Silicon computers, which means
    the path to fzf should not be hard coded.

--- zshrc.sh
+++ zshrc.sh
@@ -73,10 +73,7 @@ DISABLE_AUTO_TITLE="true"
 #
 
 source $HOME/.zsh/bundles/zsh-autosuggestions/zsh-autosuggestions.zsh
-if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
-  source /usr/local/opt/fzf/shell/key-bindings.zsh
-  source /usr/local/opt/fzf/shell/completion.zsh
-fi
+
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
 
@@ -94,6 +91,16 @@ source $HOME/.zsh/path.zsh
 #
 # FZF
 #
+
+if _has brew; then
+  BREWPREFIX=$(brew --prefix)
+fi
+
+if [ -e $BREWPREFIX/opt/fzf/shell/completion.zsh ]; then
+  source $BREWPREFIX/opt/fzf/shell/key-bindings.zsh
+  source $BREWPREFIX/opt/fzf/shell/completion.zsh
+fi
+
 if _has fzf && _has rg; then
   export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!{.git,node_modules}/*" 2> /dev/null'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
