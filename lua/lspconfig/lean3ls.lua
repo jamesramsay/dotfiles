10b21254edb763c7de7c0e62fd7d127f197f34ac
James Ramsay <james@jramsay.com.au>
2021-07-25 19:33:28 +1000

    Squashed 'vim/pack/bundle/opt/nvim-lspconfig/' content from commit 00028be
    git-subtree-dir: vim/pack/bundle/opt/nvim-lspconfig
    git-subtree-split: 00028be5527abbc829ae8263cb3319dcae08e8ec

---
+++ lua/lspconfig/lean3ls.lua
@@ -0,0 +1,41 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+configs.lean3ls = {
+  default_config = {
+    cmd = { 'lean-language-server', '--stdio', '--', '-M', '4096', '-T', '100000' },
+    filetypes = { 'lean3' },
+    root_dir = function(fname)
+      return util.root_pattern 'leanpkg.toml'(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end,
+    on_new_config = function(config, root)
+      if not util.path.is_file(root .. '/leanpkg.toml') then
+        return
+      end
+      if not config.cmd_cwd then
+        config.cmd_cwd = root
+      end
+    end,
+  },
+  docs = {
+    description = [[
+https://github.com/leanprover/lean-client-js/tree/master/lean-language-server
+
+Lean installation instructions can be found
+[here](https://leanprover-community.github.io/get_started.html#regular-install).
+
+Once Lean is installed, you can install the Lean 3 language server by running
+```sh
+npm install -g lean-language-server
+```
+
+Note: that if you're using [lean.nvim](https://github.com/Julian/lean.nvim),
+that plugin fully handles the setup of the Lean language server,
+and you shouldn't set up `lean3ls` both with it and `lspconfig`.
+    ]],
+    default_config = {
+      root_dir = [[root_pattern("leanpkg.toml") or root_pattern(".git") or path.dirname]],
+    },
+  },
+}
+-- vim:et ts=2 sw=2
